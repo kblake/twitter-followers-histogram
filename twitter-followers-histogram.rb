b9511d74d3ad0cb07898985f794dde6cedbcb7c5
Karmen Blake <karmenblake@gmail.com>
2010-09-18 17:51:29 -0700

    limited follower count to 50

--- twitter-followers-histogram.rb
+++ twitter-followers-histogram.rb
@@ -1,11 +1,13 @@
 require 'twitter'
-require './credentials'
 
+size = 50 #limit to 50 followers arbitrarily
 
-oauth = Twitter::OAuth.new(CONSUMER_TOKEN, CONSUMER_SECRET)
-access_token = oauth.request_token.token
-access_secret = oauth.request_token.secret
-oauth.authorize_from_access(access_token, access_secret)
-client = Twitter::Base.new(oauth)
-p client
+ids = Twitter.follower_ids('kblake')[0..size-1]
+
+ids.each do |id|
+	user = Twitter.user(id)
+	count = user.followers_count
+	puts "#{user.name}: (#{count}) " + "*" * count
+	puts
+end
 
